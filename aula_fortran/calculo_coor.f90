ed92661c475dfa43d6538473c195a22a4595234b
Wendell Nobre <60453323+wendell8@users.noreply.github.com>
2023-03-25 20:22:59 -0300

    Add files via upload

--- aula_fortran/calculo_coor.f90
+++ aula_fortran/calculo_coor.f90
@@ -1,17 +1,47 @@
-program coordenada
+program main
     implicit none
-    real:: c, f, g, h, d
-
-    write(*,*) 'Qual o valor de X1 e X2, respectivamente?'
-    read(*,*)c,f
-    write(*,*) 'Qual o valor de Y1 e Y2, respectivamente?'
-    read(*,*)g,h
-
-    d = sqrt(((f-c)**2)+((h-g)**2))
-
-
-    write(*,*)'A distância entre os pontos é:', d ,'m'
     
-
-
-end program coordenada
+    integer :: x1, x2, y1, y2
+    real :: area, a, b, c, d
+  
+    print*, "Entre com as coordenadas x e y do primeiro ponto:"
+    read*, x1, y1
+  
+    print*, "Entre com as coordenadas x e y do segundo ponto:"
+    read*, x2, y2
+  
+    d = sqrt(REAL((x2 - x1)**2 + (y2 - y1)**2))
+    print*, "A distância entre os pontos é: ", d
+  
+    print*, "Entre com as medidas dos lados do triângulo:"
+    read*, a, b, c
+  
+    area = triangle_area(a, b, c)
+  
+    if (area < 0) then
+      print*, "As medidas dos lados não formam um triângulo válido!"
+    else
+      print*, "A área do triângulo é: ", area
+    endif
+  
+  contains
+  
+    function triangle_area(a, b, c) result(area)
+      implicit none
+      real, intent(in) :: a, b, c
+      real :: s, area, discriminant
+  
+      if (a + b <= c .or. a + c <= b .or. b + c <= a) then
+        area = -1
+      else
+        s = (a + b + c) / 2
+        discriminant = s * (s - a) * (s - b) * (s - c)
+        if (discriminant < 0) then
+          area = -1
+        else
+          area = sqrt(discriminant)
+        endif
+      endif
+    end function triangle_area
+  
+  end program main 
