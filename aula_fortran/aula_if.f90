ead3d3ac389994df4cb9a3f3708d644bf53be848
Wendell Nobre <60453323+wendell8@users.noreply.github.com>
2023-03-28 10:01:49 -0300

    Update aula_if.f90

--- aula_fortran/aula_if.f90
+++ aula_fortran/aula_if.f90
@@ -1,13 +1,18 @@
 real function chapeu(x,a)
 implicit none
 real,intent(in):: x,a
+
 if(x<a) then
     chapeu = 0.
+    
 else if (x<0) then
     chapeu = (a-x)/a
+    
 else if (x<a)then
     chapeu = (a-x)/a 
+    
 else
     chapeu = 0.
+    
 end if
 end function chapeu
